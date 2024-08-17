3bf0114350a579421ad44440e48e84deb5199529
Jennifer <87148355+JenniferSieber@users.noreply.github.com>
2024-08-17 16:32:53 -0600

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -4,27 +4,21 @@ import puppeteer from 'puppeteer';
 const scrape = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-
   const allBooks = [];
   let currentPage = 1;
-  const maxPages = 10;
+  const maxPages = 50;
 
   while (currentPage <= maxPages) {
     const url = `https://books.toscrape.com/catalogue/page-${currentPage}.html`;
-
     await page.goto(url);
-
+    
     const books = await page.evaluate(() => {
       const bookElements = document.querySelectorAll('.product_pod');
       return Array.from(bookElements).map((book) => {
         const title = book.querySelector('h3 a').getAttribute('title');
         const price = book.querySelector('.price_color').textContent;
-        const stock = book.querySelector('.instock.availability')
-          ? 'In Stock'
-          : 'Out Of Stock';
-        const rating = book
-          .querySelector('.star-rating')
-          .className.split(' ')[1];
+        const stock = book.querySelector('.instock.availability') ? 'In Stock' : 'Out Of Stock';
+        const rating = book.querySelector('.star-rating').className.split(' ')[1];
         const link = book.querySelector('h3 a').getAttribute('href');
 
         return {
@@ -43,9 +37,7 @@ const scrape = async () => {
   }
 
   fs.writeFileSync('books.json', JSON.stringify(allBooks, null, 2));
-
   console.log('Data saved to books.json');
-
   await browser.close();
 };
 
